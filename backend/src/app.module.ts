68bc7687b18e9b4f25697eec328739195a7f1099
Priyanshu Kumar Verma <priyanshu76670@gmail.com>
2026-08-13 13:38:35 +0530

    chore: connect sqlite database via typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'database.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
