5ea785cc37236d1da43711ee0e33e4b49901c232
Priyanshu Kumar Verma <priyanshu76670@gmail.com>
2026-08-13 15:51:05 +0530

    feat: add task entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,16 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './tasks/entitites/task.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'better-sqlite3',
       database: 'database.sqlite',
-      entities: [],
+      entities: [Task],
       synchronize: true,
     }),
   ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/tasks/entitites/task.entity.ts
@@ -0,0 +1,37 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+
+@Entity('tasks')
+export class Task {
+    // fields go here
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+    @Column()
+    title!: string;
+
+    @Column({ nullable: true })
+    description!: string;
+
+    @Column({ default: 'todo' })
+    status!: string;
+
+    @Column({ default: 'no_priority' })
+    priority!: string;
+
+    @Column({ nullable: true, type: 'datetime' })
+    dueDate!: Date;
+    
+    @CreateDateColumn()
+    createdAt!: Date;
+
+    @UpdateDateColumn()
+    updatedAt!: Date;
+
+}
