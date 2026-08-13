dc1d8a7d1a26511a6b3a8bb2043c5d1c9ebd446a
Priyanshu Kumar Verma <priyanshu76670@gmail.com>
2026-08-13 16:35:08 +0530

    feat: add get all tasks endpoint

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './entities/task.entity';
+
 
 @Injectable()
 export class TasksService {
+  constructor(
+    @InjectRepository(Task)
+    private tasksRepository: Repository<Task>,
+  ) {}
+
+  // ...existing methods below
   create(createTaskDto: CreateTaskDto) {
     return 'This action adds a new task';
   }
 
-  findAll() {
-    return `This action returns all tasks`;
-  }
+  async findAll() {
+  return this.tasksRepository.find();
+}
 
   findOne(id: number) {
     return `This action returns a #${id} task`;
