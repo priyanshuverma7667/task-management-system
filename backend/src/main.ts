4be3543e922d6b747b67fa2bc83641143f19890a
Priyanshu Kumar Verma <priyanshu76670@gmail.com>
2026-08-20 13:43:04 +0530

    fix: allow deployed frontend origin in cors config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,9 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  });
+  origin: [
+    'http://localhost:3000',
+    'https://task-management-system-je6itha18-smart-task-management.vercel.app',
+  ],
+  credentials: true,
+});
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
